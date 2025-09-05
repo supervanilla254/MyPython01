08d5b224ffc32a1f9795b3f94b0af511e967516d
Supervanilla254 <mmmnd01@gmail.com>
2025-09-05 12:06:32 +0700

    v8

---
+++ test29.py
@@ -0,0 +1,28 @@
+# 1.function: no parameter, no return
+# parameter: คือตัวแปรประเภทหนึ่ง ที่ใช้ได้เฉพาะในฟังก์ชันนั้นๆ เท่านั้น
+# return: คือคำสั่งที่บ่งบอกถึงการจบการทำงานของฟังก์ชั่นนั้นๆ และส่งค่ากลับไปยังจุดที่เรียกใช้ฟังก์ชั่นนั้น(ถ้ามี)
+
+...
+def func_name() :
+    "คำสั่ง"
+    "คำสั่ง"
+    "........."
+...
+print('wow')
+
+def showHi() :
+    print('Hi')
+
+print('Wo')
+    
+def showHey() :
+    print('Hey')
+
+print('Wee')
+
+showHi()
+showHi()
+showHey()
+showHi()
+    
+   
